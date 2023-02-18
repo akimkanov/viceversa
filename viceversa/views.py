d44ef7e34a6f9444e2463f9cdac691316494c24d
Yryskeldi <akimkanov7@gmail.com>
2023-02-18 21:15:08 +0600

    Add words len feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,7 +4,8 @@ def home(request):
     return render(request, 'home.html')
 
 def reverse(request):
-    user_text = request.GET['user-text']
+    user_text = request.GET['usertext']
     reverse_text = user_text[::-1]
+    text_len = len(user_text.split())
     
-    return render(request, 'reverse.html', {'usertext':user_text, 'reversed_text':reverse_text})
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversed_text':reverse_text, 'len_text':text_len} )
